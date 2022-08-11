8498c1f563e13d80a5a34594a9fcb605949c4f49
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2022-08-11 16:22:17 +0000

    Deploying to gh-pages from @ danondanissimo/team-project@e9cf45c043407d79c1242a5dfa4514b96bbd3251 🚀

--- index.4b37c8d8.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[link-menu]"),t=document.querySelector("[data-menu]"),n=document.querySelector("[data-menu-button]");e.addEventListener("click",(function(){n.classList.toggle("is-open"),t.classList.toggle("is-open")}))}();
-//# sourceMappingURL=index.4b37c8d8.js.map
---
+++ index.577a5828.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[link-menu]"),t=document.querySelector("[data-menu]"),o=document.querySelector("[data-menu-button]"),n=document.querySelector("[data-body]");e.addEventListener("click",(function(){o.classList.toggle("is-open"),t.classList.toggle("is-open"),n.classList.toggle("no-scroll")}))}();
+//# sourceMappingURL=index.577a5828.js.map
